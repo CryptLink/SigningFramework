30b86fd3edc8538498993a260040b6c6d27bac2a
Jeremy Peterson <jeremy.one@gmail.com>
2018-06-02 09:16:27 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,8 @@
 dotnet --version;
 dotnet restore SigningFramework.csproj --verbosity m;
 dotnet restore Tests/SigningFrameworkTests.csproj --verbosity m;
-dotnet test Tests/SigningFrameworkTests.csproj -xml SigningFrameworkTests.xml;
+dotnet test Tests/SigningFrameworkTests.csproj --logger:"trx;[LogFileName=Results.trx]";
       
 # upload results to AppVeyor
 $wc = New-Object 'System.Net.WebClient';
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/xunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\SigningFrameworkTests.xml));
+$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\Results.trx));
