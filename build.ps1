4e52692ff3343a1316f11428f59c93b120ef6ac6
Jeremy Peterson <jeremy.one@gmail.com>
2018-06-03 09:26:40 -0700

    Getting tests working in build.ps1

--- build.ps1
+++ build.ps1
@@ -1,8 +1,10 @@
 dotnet --version;
 dotnet restore SigningFramework.csproj --verbosity m;
 dotnet restore Tests/SigningFrameworkTests.csproj --verbosity m;
-dotnet test Tests/SigningFrameworkTests.csproj --logger:"trx;[LogFileName=Results.trx]";
+dotnet test Tests/SigningFrameworkTests.csproj --logger:"trx;LogFileName=\Results.trx";
       
-# upload results to AppVeyor
-$wc = New-Object 'System.Net.WebClient';
-$wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\Results.trx));
+if ($env:APPVEYOR -eq "True") {
+    # upload results to AppVeyor
+    $wc = New-Object 'System.Net.WebClient';
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path Tests/TestResults/Results.trx));
+}
